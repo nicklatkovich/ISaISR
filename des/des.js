8a9c12acf725be925f9b2c1653ce120d4982d7e6
NickLatkovich <nicklatkovich@gmail.com>
2018-01-05 04:25:17 +0300

    Des refactoring

--- des/des.js
+++ des/des.js
@@ -215,73 +215,91 @@ function des(key, message, encrypt, mode, iv, padding) {
 }
 
 function des_createKeys(key) {
-    pc2bytes0 = new Array(0, 0x4, 0x20000000, 0x20000004, 0x10000, 0x10004, 0x20010000, 0x20010004, 0x200, 0x204, 0x20000200, 0x20000204, 0x10200, 0x10204, 0x20010200, 0x20010204);
-    pc2bytes1 = new Array(0, 0x1, 0x100000, 0x100001, 0x4000000, 0x4000001, 0x4100000, 0x4100001, 0x100, 0x101, 0x100100, 0x100101, 0x4000100, 0x4000101, 0x4100100, 0x4100101);
-    pc2bytes2 = new Array(0, 0x8, 0x800, 0x808, 0x1000000, 0x1000008, 0x1000800, 0x1000808, 0, 0x8, 0x800, 0x808, 0x1000000, 0x1000008, 0x1000800, 0x1000808);
-    pc2bytes3 = new Array(0, 0x200000, 0x8000000, 0x8200000, 0x2000, 0x202000, 0x8002000, 0x8202000, 0x20000, 0x220000, 0x8020000, 0x8220000, 0x22000, 0x222000, 0x8022000, 0x8222000);
-    pc2bytes4 = new Array(0, 0x40000, 0x10, 0x40010, 0, 0x40000, 0x10, 0x40010, 0x1000, 0x41000, 0x1010, 0x41010, 0x1000, 0x41000, 0x1010, 0x41010);
-    pc2bytes5 = new Array(0, 0x400, 0x20, 0x420, 0, 0x400, 0x20, 0x420, 0x2000000, 0x2000400, 0x2000020, 0x2000420, 0x2000000, 0x2000400, 0x2000020, 0x2000420);
-    pc2bytes6 = new Array(0, 0x10000000, 0x80000, 0x10080000, 0x2, 0x10000002, 0x80002, 0x10080002, 0, 0x10000000, 0x80000, 0x10080000, 0x2, 0x10000002, 0x80002, 0x10080002);
-    pc2bytes7 = new Array(0, 0x10000, 0x800, 0x10800, 0x20000000, 0x20010000, 0x20000800, 0x20010800, 0x20000, 0x30000, 0x20800, 0x30800, 0x20020000, 0x20030000, 0x20020800, 0x20030800);
-    pc2bytes8 = new Array(0, 0x40000, 0, 0x40000, 0x2, 0x40002, 0x2, 0x40002, 0x2000000, 0x2040000, 0x2000000, 0x2040000, 0x2000002, 0x2040002, 0x2000002, 0x2040002);
-    pc2bytes9 = new Array(0, 0x10000000, 0x8, 0x10000008, 0, 0x10000000, 0x8, 0x10000008, 0x400, 0x10000400, 0x408, 0x10000408, 0x400, 0x10000400, 0x408, 0x10000408);
-    pc2bytes10 = new Array(0, 0x20, 0, 0x20, 0x100000, 0x100020, 0x100000, 0x100020, 0x2000, 0x2020, 0x2000, 0x2020, 0x102000, 0x102020, 0x102000, 0x102020);
-    pc2bytes11 = new Array(0, 0x1000000, 0x200, 0x1000200, 0x200000, 0x1200000, 0x200200, 0x1200200, 0x4000000, 0x5000000, 0x4000200, 0x5000200, 0x4200000, 0x5200000, 0x4200200, 0x5200200);
-    pc2bytes12 = new Array(0, 0x1000, 0x8000000, 0x8001000, 0x80000, 0x81000, 0x8080000, 0x8081000, 0x10, 0x1010, 0x8000010, 0x8001010, 0x80010, 0x81010, 0x8080010, 0x8081010);
-    pc2bytes13 = new Array(0, 0x4, 0x100, 0x104, 0, 0x4, 0x100, 0x104, 0x1, 0x5, 0x101, 0x105, 0x1, 0x5, 0x101, 0x105);
-
-    //how many iterations (1 for des, 3 for triple des)
-    var iterations = key.length > 8 ? 3 : 1; //changed by Paul 16/6/2007 to use Triple DES for 9+ byte keys
-    //stores the return keys
+    pc2bytes0 = new Array(0, 0x4, 0x20000000, 0x20000004, 0x10000, 0x10004, 0x20010000, 0x20010004, 0x200, 0x204,
+        0x20000200, 0x20000204, 0x10200, 0x10204, 0x20010200, 0x20010204);
+    pc2bytes1 = new Array(0, 0x1, 0x100000, 0x100001, 0x4000000, 0x4000001, 0x4100000, 0x4100001, 0x100, 0x101,
+        0x100100, 0x100101, 0x4000100, 0x4000101, 0x4100100, 0x4100101);
+    pc2bytes2 = new Array(0, 0x8, 0x800, 0x808, 0x1000000, 0x1000008, 0x1000800, 0x1000808, 0, 0x8, 0x800, 0x808,
+        0x1000000, 0x1000008, 0x1000800, 0x1000808);
+    pc2bytes3 = new Array(0, 0x200000, 0x8000000, 0x8200000, 0x2000, 0x202000, 0x8002000, 0x8202000, 0x20000,
+        0x220000, 0x8020000, 0x8220000, 0x22000, 0x222000, 0x8022000, 0x8222000);
+    pc2bytes4 = new Array(0, 0x40000, 0x10, 0x40010, 0, 0x40000, 0x10, 0x40010, 0x1000, 0x41000, 0x1010, 0x41010,
+        0x1000, 0x41000, 0x1010, 0x41010);
+    pc2bytes5 = new Array(0, 0x400, 0x20, 0x420, 0, 0x400, 0x20, 0x420, 0x2000000, 0x2000400, 0x2000020,
+        0x2000420, 0x2000000, 0x2000400, 0x2000020, 0x2000420);
+    pc2bytes6 = new Array(0, 0x10000000, 0x80000, 0x10080000, 0x2, 0x10000002, 0x80002, 0x10080002, 0, 0x10000000,
+        0x80000, 0x10080000, 0x2, 0x10000002, 0x80002, 0x10080002);
+    pc2bytes7 = new Array(0, 0x10000, 0x800, 0x10800, 0x20000000, 0x20010000, 0x20000800, 0x20010800, 0x20000,
+        0x30000, 0x20800, 0x30800, 0x20020000, 0x20030000, 0x20020800, 0x20030800);
+    pc2bytes8 = new Array(0, 0x40000, 0, 0x40000, 0x2, 0x40002, 0x2, 0x40002, 0x2000000, 0x2040000, 0x2000000,
+        0x2040000, 0x2000002, 0x2040002, 0x2000002, 0x2040002);
+    pc2bytes9 = new Array(0, 0x10000000, 0x8, 0x10000008, 0, 0x10000000, 0x8, 0x10000008, 0x400, 0x10000400,
+        0x408, 0x10000408, 0x400, 0x10000400, 0x408, 0x10000408);
+    pc2bytes10 = new Array(0, 0x20, 0, 0x20, 0x100000, 0x100020, 0x100000, 0x100020, 0x2000, 0x2020, 0x2000,
+        0x2020, 0x102000, 0x102020, 0x102000, 0x102020);
+    pc2bytes11 = new Array(0, 0x1000000, 0x200, 0x1000200, 0x200000, 0x1200000, 0x200200, 0x1200200, 0x4000000,
+        0x5000000, 0x4000200, 0x5000200, 0x4200000, 0x5200000, 0x4200200, 0x5200200);
+    pc2bytes12 = new Array(0, 0x1000, 0x8000000, 0x8001000, 0x80000, 0x81000, 0x8080000, 0x8081000, 0x10, 0x1010,
+        0x8000010, 0x8001010, 0x80010, 0x81010, 0x8080010, 0x8081010);
+    pc2bytes13 = new Array(0, 0x4, 0x100, 0x104, 0, 0x4, 0x100, 0x104, 0x1, 0x5, 0x101, 0x105, 0x1, 0x5, 0x101,
+        0x105);
+    var iterations = key.length > 8 ? 3 : 1;
     var keys = new Array(32 * iterations);
-    //now define the left shifts which need to be done
     var shifts = new Array(0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0);
-    //other variables
     var lefttemp, righttemp, m = 0, n = 0, temp;
-
-    for (var j = 0; j < iterations; j++) { //either 1 or 3 iterations
-        left = (key.charCodeAt(m++) << 24) | (key.charCodeAt(m++) << 16) | (key.charCodeAt(m++) << 8) | key.charCodeAt(m++);
-        right = (key.charCodeAt(m++) << 24) | (key.charCodeAt(m++) << 16) | (key.charCodeAt(m++) << 8) | key.charCodeAt(m++);
-
-        temp = ((left >>> 4) ^ right) & 0x0f0f0f0f; right ^= temp; left ^= (temp << 4);
-        temp = ((right >>> -16) ^ left) & 0x0000ffff; left ^= temp; right ^= (temp << -16);
-        temp = ((left >>> 2) ^ right) & 0x33333333; right ^= temp; left ^= (temp << 2);
-        temp = ((right >>> -16) ^ left) & 0x0000ffff; left ^= temp; right ^= (temp << -16);
-        temp = ((left >>> 1) ^ right) & 0x55555555; right ^= temp; left ^= (temp << 1);
-        temp = ((right >>> 8) ^ left) & 0x00ff00ff; left ^= temp; right ^= (temp << 8);
-        temp = ((left >>> 1) ^ right) & 0x55555555; right ^= temp; left ^= (temp << 1);
-
-        //the right side needs to be shifted and to get the last four bits of the left side
+    for (var j = 0; j < iterations; j++) {
+        left =
+            (key.charCodeAt(m++) << 24) | (key.charCodeAt(m++) << 16) | (key.charCodeAt(m++) << 8) |
+            key.charCodeAt(m++);
+        right =
+            (key.charCodeAt(m++) << 24) | (key.charCodeAt(m++) << 16) | (key.charCodeAt(m++) << 8) |
+            key.charCodeAt(m++);
+        temp = ((left >>> 4) ^ right) & 0x0f0f0f0f;
+        right ^= temp;
+        left ^= (temp << 4);
+        temp = ((right >>> -16) ^ left) & 0x0000ffff;
+        left ^= temp;
+        right ^= (temp << -16);
+        temp = ((left >>> 2) ^ right) & 0x33333333;
+        right ^= temp;
+        left ^= (temp << 2);
+        temp = ((right >>> -16) ^ left) & 0x0000ffff;
+        left ^= temp;
+        right ^= (temp << -16);
+        temp = ((left >>> 1) ^ right) & 0x55555555;
+        right ^= temp;
+        left ^= (temp << 1);
+        temp = ((right >>> 8) ^ left) & 0x00ff00ff;
+        left ^= temp;
+        right ^= (temp << 8);
+        temp = ((left >>> 1) ^ right) & 0x55555555;
+        right ^= temp;
+        left ^= (temp << 1);
         temp = (left << 8) | ((right >>> 20) & 0x000000f0);
-        //left needs to be put upside down
         left = (right << 24) | ((right << 8) & 0xff0000) | ((right >>> 8) & 0xff00) | ((right >>> 24) & 0xf0);
         right = temp;
-
-        //now go through and perform these shifts on the left and right keys
         for (i = 0; i < shifts.length; i++) {
-            //shift the keys either one or two bits to the left
-            if (shifts[i]) { left = (left << 2) | (left >>> 26); right = (right << 2) | (right >>> 26); }
-            else { left = (left << 1) | (left >>> 27); right = (right << 1) | (right >>> 27); }
-            left &= -0xf; right &= -0xf;
-
-            //now apply PC-2, in such a way that E is easier when encrypting or decrypting
-            //this conversion will look like PC-2 except only the last 6 bits of each byte are used
-            //rather than 48 consecutive bits and the order of lines will be according to 
-            //how the S selection functions will be applied: S2, S4, S6, S8, S1, S3, S5, S7
-            lefttemp = pc2bytes0[left >>> 28] | pc2bytes1[(left >>> 24) & 0xf]
-                | pc2bytes2[(left >>> 20) & 0xf] | pc2bytes3[(left >>> 16) & 0xf]
-                | pc2bytes4[(left >>> 12) & 0xf] | pc2bytes5[(left >>> 8) & 0xf]
-                | pc2bytes6[(left >>> 4) & 0xf];
-            righttemp = pc2bytes7[right >>> 28] | pc2bytes8[(right >>> 24) & 0xf]
-                | pc2bytes9[(right >>> 20) & 0xf] | pc2bytes10[(right >>> 16) & 0xf]
-                | pc2bytes11[(right >>> 12) & 0xf] | pc2bytes12[(right >>> 8) & 0xf]
-                | pc2bytes13[(right >>> 4) & 0xf];
+            if (shifts[i]) {
+                left = (left << 2) | (left >>> 26);
+                right = (right << 2) | (right >>> 26);
+            } else {
+                left = (left << 1) | (left >>> 27);
+                right = (right << 1) | (right >>> 27);
+            }
+            left &= -0xf;
+            right &= -0xf;
+            lefttemp =
+                pc2bytes0[left >>> 28] | pc2bytes1[(left >>> 24) & 0xf] | pc2bytes2[(left >>> 20) & 0xf] |
+                pc2bytes3[(left >>> 16) & 0xf] | pc2bytes4[(left >>> 12) & 0xf] | pc2bytes5[(left >>> 8) & 0xf] |
+                pc2bytes6[(left >>> 4) & 0xf];
+            righttemp =
+                pc2bytes7[right >>> 28] | pc2bytes8[(right >>> 24) & 0xf] | pc2bytes9[(right >>> 20) & 0xf] |
+                pc2bytes10[(right >>> 16) & 0xf] | pc2bytes11[(right >>> 12) & 0xf] |
+                pc2bytes12[(right >>> 8) & 0xf] | pc2bytes13[(right >>> 4) & 0xf];
             temp = ((righttemp >>> 16) ^ lefttemp) & 0x0000ffff;
-            keys[n++] = lefttemp ^ temp; keys[n++] = righttemp ^ (temp << 16);
+            keys[n++] = lefttemp ^ temp;
+            keys[n++] = righttemp ^ (temp << 16);
         }
-    } //for each iterations
-    //return the keys we've created
+    }
     return keys;
-} //end of des_createKeys
-
-
+}
